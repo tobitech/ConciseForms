6af7db578ebe179ed1e20ed4bcc5cdfc2c7c5398
Oluwatobi Omotayo <tobitech@ymail.com>
2022-06-30 13:11:47 +0100

    Initial Commit

---
+++ ConciseForms/ConciseFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConciseFormsApp.swift
+//  ConciseForms
+//
+//  Created by Oluwatobi Omotayo on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConciseFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
